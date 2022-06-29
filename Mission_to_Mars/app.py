22626c40085f35ebce00f503ed3bee1f133db6c4
Fenny Patel <fennypatel02@gmail.com>
2022-06-28 20:19:29 -0700

    final update

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -6,9 +6,10 @@ from flask_pymongo import PyMongo
 
 # Create an instance of Flask
 app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
-# Use PyMongo to establish Mongo connection
 #conn = 'mongodb://localhost:27017'
 #client = pymongo.MongoClient(conn)
 # Create database
